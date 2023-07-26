0d9bdedf358263394061eeb59667de00361ae4ac
RapaN <kostyasteamonly@gmail.com>
2023-07-26 19:47:06 +0300

    add images

--- src/utils/productsArray.ts
+++ src/utils/productsArray.ts
@@ -1,4 +1,6 @@
 type Product = {
+    id: number
+    image: string
     title: string
     description: string
     type: string
@@ -8,46 +10,58 @@ type Product = {
 
 const productsArray:Product[] = [
     {
+        id: 1,
         title: 'iPhone 14 Pro',
         description: 'This is iPhone  Pro',
         type: 'phone',
         capacity: '64',
         price: 1000,
+        image: '/images/Iphone-black.webp',
     },
     {
+        id: 2,
         title: 'iPhone 13 Pro',
         description: 'This is iPhone Pro',
         type: 'phone',
         capacity: '12',
         price: 555,
+        image: '/images/Iphone-green.webp',
     },
     {
+        id: 3,
         title: 'iPhone 12 Pro',
         description: 'This is iPhone Pro',
         type: 'phone',
         capacity: '15',
         price: 870,
+        image: '/images/Iphone-grey.webp',
     },
     {
+        id: 4,
         title: 'iPhone 11 ',
         description: 'This is iPhone  ',
         type: 'phone',
         capacity: '123',
         price: 667,
+        image: '/images/Iphone-purple.webp',
     },
     {
+        id: 5,
         title: 'iPhone 10 Pro',
         description: 'This is iPhone Extra',
         type: 'phone',
         capacity: '642',
         price: 850,
+        image: '/images/Iphone-red.webp',
     },
     {
+        id: 6,
         title: 'iPhone 15 Pro',
         description: 'This is iPhone 15 Pro',
         type: 'phone',
         capacity: '128',
         price: 1200,
+        image: '/images/Iphone-white.webp',
     },
 ]
 
