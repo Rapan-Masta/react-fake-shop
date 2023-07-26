26af8dd69224de308e2040dcbc39805d75311f53
RapaN <kostyasteamonly@gmail.com>
2023-07-26 18:41:15 +0300

    add type Props and some info for mobile

--- src/components/Products/ProductList.tsx
+++ src/components/Products/ProductList.tsx
@@ -14,13 +14,31 @@ const ProductList = (props: Props) => {
             </Typography>
             <Grid container spacing={4}>
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductListItem />
+                    <ProductListItem
+                        title="iPhone 14 Pro"
+                        description="This is iPhone 14 Pro"
+                        type="phone"
+                        capacity="64"
+                        price={1000}
+                    />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductListItem />
+                    <ProductListItem
+                        title="iPhone 13"
+                        description="This is iPhone 13"
+                        type="phone"
+                        capacity="32"
+                        price={800}
+                    />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductListItem />
+                    <ProductListItem
+                        title="iPhone 12 Extra"
+                        description="This is iPhone 12 Extra"
+                        type="phone"
+                        capacity="16"
+                        price={500}
+                    />
                 </Grid>
             </Grid>
         </div>
